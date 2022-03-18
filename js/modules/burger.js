cf47e54a0d4ea5ae0cee8efd107682ded6ba0948
Konstantin <laedo303@mail.ru>
2022-03-18 20:41:55 +0500

    fix_burger

--- js/modules/burger.js
+++ js/modules/burger.js
@@ -2,10 +2,7 @@ export const burger = () => {
   const menu = document.querySelector('.header__menu');
 
   document.body.addEventListener('click', (e) => {
-    const target = e.target;
-    console.log('target: ', target);
-
-    if (target.classList.contains('header__menu-button')) {
+    if (e.target.classList.contains('header__menu-button')) {
       menu.classList.toggle('header__menu_active');
     } else {
       menu.classList.remove('header__menu_active');
