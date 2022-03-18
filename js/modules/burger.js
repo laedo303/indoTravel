e6b10475359fbad7f979919b4a07b2c039c197de
Konstantin <laedo303@mail.ru>
2022-03-18 20:13:20 +0500

    fix_burger

--- js/modules/burger.js
+++ js/modules/burger.js
@@ -1,10 +1,14 @@
 export const burger = () => {
-  console.log('burger');
-
-  const btnOpen = document.querySelector('.header__menu-button');
   const menu = document.querySelector('.header__menu');
 
-  btnOpen.addEventListener('click', () => {
-    menu.classList.toggle('header__menu_active');
+  document.body.addEventListener('click', (e) => {
+    const target = e.target;
+    console.log('target: ', target);
+
+    if (target.classList.contains('header__menu-button')) {
+      menu.classList.toggle('header__menu_active');
+    } else {
+      menu.classList.remove('header__menu_active');
+    }
   });
 };
