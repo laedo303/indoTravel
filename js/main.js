5cfdb10609dc50fbeba5ea47d179fac9a4b15918
Konstantin <laedo303@mail.ru>
2022-03-31 02:05:43 +0500

    renderTours

--- js/main.js
+++ js/main.js
@@ -3,7 +3,6 @@ import {acc} from './modules/acc.js';
 import {burger} from './modules/burger.js';
 import {fly} from './modules/fly.js';
 import {renderTours} from './modules/renderTours.js';
-// import './modules/tours.js';
 
 
 {
